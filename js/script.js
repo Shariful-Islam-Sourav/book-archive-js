63a25c98034db9ba68357d6a7ddb9e14e53785f4
Shariful-Isla-Sourav <sorabsorab347@gmail.com>
2021-09-02 18:05:30 +0600

    Search & Book Results

--- js/script.js
+++ js/script.js
@@ -5,14 +5,24 @@ const searchBook = () => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => showBooks(data.docs));
+  searchInput.value = "";
 };
 
 const showBooks = (books) => {
-    const booksContainer = document.getElementById("books-container");
-    books.forEach((book) =>{
-       const bookItem = document.createElement("div")
-       bookItem.classList.add("col");
-       bookItem.innerHTML = `
+  const resultSection = document.getElementById("results-section");
+  const booksContainer = document.getElementById("books-container");
+  booksContainer.textContent = "";
+  resultSection.textContent = "";
+  //Showing Total Search Results
+  const showAmountOfBooks = document.createElement("h3");
+  showAmountOfBooks.classList.add("text-success")
+  showAmountOfBooks.innerText = `Showing ${books.length} books`;
+  resultSection.appendChild(showAmountOfBooks);
+  //Showing Book Info
+  books.forEach((book) => {
+    const bookItem = document.createElement("div");
+    bookItem.classList.add("col");
+    bookItem.innerHTML = `
        <div class="card h-100">
             <img height="100%" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
@@ -21,8 +31,8 @@ const showBooks = (books) => {
                 <p>First Publish Year- ${book.first_publish_year}</p>
             </div>
         </div>
-       `
-       booksContainer.appendChild(bookItem)
-       console.log(book);
-    })
-}
+       `;
+    booksContainer.appendChild(bookItem);
+    //console.log(book);
+  });
+};
